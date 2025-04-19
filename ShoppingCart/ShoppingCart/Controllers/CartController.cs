7c58e9bb9170440f60f069d4a1fa3c40e8499407
Ancol_Developer <trandieu026@gmail.com>
2025-04-19 23:47:47 +0700

    Add Card model Add Carrd View

--- ShoppingCart/ShoppingCart/Controllers/CartController.cs
+++ ShoppingCart/ShoppingCart/Controllers/CartController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using ShoppingCart.Models.ViewModel;
+using ShoppingCart.Repository;
 
 namespace ShoppingCart.Controllers
 {
     public class CartController : Controller
     {
+        private readonly DataContext _db;
+
+        public CartController(DataContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<CartItemModel> cartItems = HttpContext.Session.GetObjectFromJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+            CartItemViewModel cartItemViewModel = new CartItemViewModel
+            {
+                CartItems = cartItems,
+                GrandTotal = cartItems.Sum(x => x.Total)
+            };
+            return View(cartItemViewModel);
         }
     }
 }
