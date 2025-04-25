82dee047114178a6e10f7a52e60ca7c1361e054c
Ancol-Developer <trandieu026@gmail.com>
2025-04-25 11:48:36 +0700

    add user Controller

---
+++ ShoppingCart/ShoppingCart/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Repository;
+
+namespace ShoppingCart.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly DataContext _db;
+
+        public UserController(DataContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _db.Users.OrderByDescending(x => x.Id).ToListAsync();
+            return View();
+        }
+    }
+}
