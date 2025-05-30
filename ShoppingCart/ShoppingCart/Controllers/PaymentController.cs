382b1fe9b1e13f87a041712dc83fde0878c6de5a
Ancol-Developer <trandieu026@gmail.com>
2025-05-30 18:32:50 +0700

    add PaymentController

---
+++ ShoppingCart/ShoppingCart/Controllers/PaymentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using ShoppingCart.Services.Mono;
+
+namespace ShoppingCart.Controllers
+{
+	public class PaymentController : Controller
+	{
+		private readonly IMomoService _momoService;
+
+		public PaymentController(IMomoService momoService)
+        {
+			this._momoService = momoService;
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> CreatePaymentMomo(OrderInforModel model)
+		{
+			var response = await _momoService.CreatePaymentAsync(model);
+			return Redirect(response.PayUrl);
+		}
+
+
+		[HttpGet]
+		public IActionResult PaymentCallBack()
+		{
+			var response = _momoService.PaymentExecuteAsync(HttpContext.Request.Query);
+			return View(response);
+		}
+	}
+}
